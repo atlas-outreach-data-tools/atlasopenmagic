70e4bee044c75457e6414eea7395c57aa1cf06e4
Giovanni Guerrieri <giovanni.guerrieri@cern.ch>
2025-08-18 11:41:17 +0200

    chore: update tests to cover for whole codebase and add codecov
    Related to #26

--- atlasopenmagic/utils.py
+++ atlasopenmagic/utils.py
@@ -15,7 +15,7 @@ import requests
 from atlasopenmagic.metadata import get_urls
 
 
-def install_from_environment(*packages, environment_file=None):
+def install_from_environment(*packages, environment_file=None): # pragma: no cover
     """
     Install specific packages listed in an environment.yml file via pip.
 
